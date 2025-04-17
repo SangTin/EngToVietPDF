6e4681ff4d870713bd9fcaea50a5441d2273333e
Sang Nguyen <sang080304@gmail.com>
2025-04-17 17:06:09 +0000

    delete debug

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ const { translate } = require("./utils/translate");
 (async () => {
     try {
         const text = await ocr.image2text("./data/sample.png");
-        console.log(text);
         const viText = await translate(text);
-        console.log(viText);
         const pdfFile = createPDF(viText);
-        console.log("This is PDF file: " + pdfFile)
     } catch (e) {
         console.log(e);
     }
