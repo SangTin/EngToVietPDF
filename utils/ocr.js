bace039b31970141b4ace868baa5edc36905114d
nmhienbn <nmhienbn@gmail.com>
2025-04-24 14:02:09 +0700

    remove cache management

--- utils/ocr.js
+++ utils/ocr.js
@@ -1,6 +1,6 @@
 const tesseract = require("node-tesseract-ocr")
 
-async function image2text(path){
+async function image2text(path) {
   return await tesseract.recognize(path, {
     lang: "eng"
   })
